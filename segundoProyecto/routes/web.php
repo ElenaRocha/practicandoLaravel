c52fff58888ab36ec9e5c3d5be448a72063e27f0
Elena Rocha <ele_r.v@hotmail.com>
2024-09-08 22:18:26 +0200

    rutas para CRUD

--- segundoProyecto/routes/web.php
+++ segundoProyecto/routes/web.php
@@ -13,3 +13,18 @@ Route::get("/articulos", [MiController::class, 'store']);
 Route::get("/mostrar", [MiController::class, 'show']);
 Route::get("/contacto", [MiController::class, 'contactar']);
 Route::get("/galeria", [MiController::class, 'galeria']);
+Route::get("/insertar", function () {
+    DB::insert("INSERT INTO articulos (NOMBRE_ARTICULO, PRECIO, PAIS_ORIGEN, SECCION, OBSERVACIONES) VALUES (?, ?, ?, ?, ?)", ["JARRON", 15.2, "JAPON", "CERAMICA", "GANGA"]);
+});
+Route::get("/leer", function () {
+    $resultados = DB::select("SELECT * FROM articulos WHERE ID=?", [1]);
+    foreach($resultados as $articulos){
+        return $articulo->nombre_articulo;
+    }
+});
+Route::get("/actualizar", function () {
+    DB::update("UPDATE articulos SET SECCION='DECORACION' WHERE ID=?", [2]);
+});
+Route::get("/borrar", function () {
+    DB::update("DELETE FROM articulos WHERE ID=?", [2]);
+});
