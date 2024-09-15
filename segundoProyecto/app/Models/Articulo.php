5fa94cd5b995eba98f52b940e27ce8e5a7daee21
Elena Rocha <ele_r.v@hotmail.com>
2024-09-15 22:39:28 +0200

    habilitar softDeletes

--- segundoProyecto/app/Models/Articulo.php
+++ segundoProyecto/app/Models/Articulo.php
@@ -4,11 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Articulo extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
     // protected $table="misArticulos";
+
+    protected $dates = ["deleted_at"];
+
     protected $fillable = [
         "nombre_articulo",
         "precio",
