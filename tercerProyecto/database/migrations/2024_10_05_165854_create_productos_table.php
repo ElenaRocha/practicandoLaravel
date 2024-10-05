e1bce6cae6133e3ac1fed739ca059f8974ef661b
Elena Rocha <ele_r.v@hotmail.com>
2024-10-05 19:02:24 +0200

    creando tabla de productos

--- tercerProyecto/database/migrations/2024_10_05_165854_create_productos_table.php
+++ tercerProyecto/database/migrations/2024_10_05_165854_create_productos_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('productos', function (Blueprint $table) {
             $table->id();
+            $table->string("nombreArticulo");
+            $table->string("seccion");
+            $table->integer("precio");
+            $table->string("fecha");
+            $table->string("paisOrigen");
             $table->timestamps();
         });
     }
