332339f6e6f688e188150e3c66fa1795d5f04688
Elena Rocha <ele_r.v@hotmail.com>
2024-10-06 09:45:10 +0200

    plantillas pra el resto de acciones

---
+++ tercerProyecto/resources/views/productos/update.blade.php
@@ -0,0 +1,12 @@
+@extends("../layouts.plantilla")
+
+@section("cabecera")
+
+@section("contenido")
+
+    <form method="post" action="">
+        <input type="text" name="nombreArticulo">
+        <input type="submit" name="enviar" value="Enviar">
+    </form>
+
+@section("pie")
