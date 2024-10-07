8eb057388598eeaa2d7506c0caa6763e532d244d
Elena Rocha <ele_r.v@hotmail.com>
2024-10-07 22:41:45 +0200

    csrf_field estaba mal referenciada

--- tercerProyecto/resources/views/productos/create.blade.php
+++ tercerProyecto/resources/views/productos/create.blade.php
@@ -1,13 +1,15 @@
 @extends("../layouts.plantilla")
 
 @section("cabecera")
+@endsection
 
 @section("contenido")
-
     <form method="post" action="/productos">
         <input type="text" name="nombreArticulo">
-        {{csrf_field}}
+        {{ csrf_field() }}
         <input type="submit" name="enviar" value="Enviar">
     </form>
+@endsection
 
 @section("pie")
+@endsection
