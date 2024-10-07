8bbc6f2776f0636b46063fc562293ae367796380
Elena Rocha <ele_r.v@hotmail.com>
2024-10-07 22:32:54 +0200

    creando toda la estructura de rutas con un recurso

--- tercerProyecto/routes/web.php
+++ tercerProyecto/routes/web.php
@@ -3,13 +3,15 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductosController;
 
+Route::resource('/productos', ProductosController::class);
+
 Route::get('/', function () {
     return view('welcome');
 });
 
 // Route::get('/inicio', 'ProductosController@index');
-Route::get('/inicio', [ProductosController::class, 'index']);
-Route::get('/crear', [ProductosController::class, 'create']);
-Route::get('/insertar', [ProductosController::class, 'store']);
-Route::get('/actualizar', [ProductosController::class, 'update']);
-Route::get('/borrar', [ProductosController::class, 'destroy']);
+// Route::get('/inicio', [ProductosController::class, 'index']);
+// Route::get('/crear', [ProductosController::class, 'create']);
+// Route::get('/insertar', [ProductosController::class, 'store']);
+// Route::get('/actualizar', [ProductosController::class, 'update']);
+// Route::get('/borrar', [ProductosController::class, 'destroy']);
