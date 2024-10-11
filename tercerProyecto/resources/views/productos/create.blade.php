31d3717f6fb763cc3585537e44d3b5fb38f7c22d
Elena Rocha <ele_r.v@hotmail.com>
2024-10-12 00:13:09 +0200

    añadir botón de borrar

--- tercerProyecto/resources/views/productos/create.blade.php
+++ tercerProyecto/resources/views/productos/create.blade.php
@@ -39,8 +39,11 @@ INSERTAR REGISTROS
                 </td>
             </tr>
             <tr>
-                <td colspan = "2" align = "center">
+                <td>
                     <input type="submit" name="enviar" value="Enviar">
+                </td>
+                <td>
+                    <input type="reset" name="borrar" value="Borrar">
                 </td>
             </tr>
         </table>
