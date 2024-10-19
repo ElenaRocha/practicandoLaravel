33bcc10b6c508c85061774a973aca63fb4a1e201
Elena Rocha <ele_r.v@hotmail.com>
2024-10-19 23:19:17 +0200

    formulario para editar productos

--- tercerProyecto/resources/views/productos/edit.blade.php
+++ tercerProyecto/resources/views/productos/edit.blade.php
@@ -10,32 +10,32 @@ EDITAR PRODUCTO
             <tr>
                 <td>Nombre: </td>
                 <td>
-                    <input type="text" name="nombreArticulo">
+                    <input type="text" name="nombreArticulo" value="{{$producto->nombreArticulo}}">
                     {{ csrf_field() }}
                 </td>
             </tr>
             <tr>
                 <td>Sección: </td>
                 <td>
-                    <input type="text" name="seccion">
+                    <input type="text" name="seccion" value="{{$producto->seccion}}">
                 </td>
             </tr>
             <tr>
                 <td>Precio: </td>
                 <td>
-                    <input type="text" name="precio">
+                    <input type="text" name="precio" value="{{$producto->precio}}">
                 </td>
             </tr>
             <tr>
                 <td>Fecha: </td>
                 <td>
-                    <input type="text" name="fecha">
+                    <input type="text" name="fecha" value="{{$producto->fecha}}">
                 </td>
             </tr>
             <tr>
                 <td>País de origen: </td>
                 <td>
-                    <input type="text" name="paisOrigen">
+                    <input type="text" name="paisOrigen" value="{{$producto->paisOrigen}}">
                 </td>
             </tr>
             <tr>
